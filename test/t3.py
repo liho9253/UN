2bada60720748749259dd0e50b3d2551e7414fec
liho9253 <timmy89566@gmail.com>
2022-03-17 17:10:50 +0800

    20220317

--- test/t3.py
+++ test/t3.py
@@ -1,17 +1,23 @@
 from selenium import webdriver
 import time
-
-
+import requests
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 path = r"C:/Program Files/python/chromedriver.exe"
 driver = webdriver.Chrome(path)
 
-time.sleep(1)
 driver.get("https://www.acc.com.tw/investor-realtions/financial-information")
 
-tables = driver.find_element_by_xpath("//*[@id='revenue']/div/table/tbody")
-# for tr in lists:
-tables_tr = tables.find_element_by_xpath("//*[@id='revenue']/div/table/tbody/tr")
-print(tables_tr[0].text)
-time.sleep(1)
+# search = {'k': "亞泥 銷售資料"}
+
+    
+d1 = driver.find_element_by_id("revenue").find_element_by_class_name("table-container")
+d2 = d1.find_element_by_tag_name("table").find_element_by_tag_name("thead").find_elements_by_tag_name("tr")[1].find_elements_by_tag_name("th")
+with open("text.txt", "a") as f:
+    for i in d2:
+        f.write(i.text)
+        f.write("\n")
 driver.quit()
