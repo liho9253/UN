d8980fb0dd850111efedea71b5983c813463cd55
liho9253 <timmy89566@gmail.com>
2022-03-25 21:04:49 +0800

    20220325

--- test/auto.py
+++ test/auto.py
@@ -9,33 +9,34 @@ import time
 path = r"C:/Program Files/python/chromedriver.exe"
 driver = webdriver.Chrome(path)
 
-def open_f(): 
-    f = open('posititon.txt', 'r', encoding='UTF-8')
+def Task():
+    f = open('shitwait.txt', 'r', encoding='UTF-8')
     msg = []
+    
     for line in f:
         s = line.split(",")
         for i in range(0,len(s)):
             msg.append(s[i])
-    f.close()
-    
-def Task():
-    driver.get("https://www.google.com/")
-    search = driver.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
-    search.clear()
-    search.send_keys("天府穴")
-    search.send_keys(Keys.RETURN)
+    print(len(msg))
+    print(msg[105])
+    driver.get("http://yibian.hopto.org/acu/")
+    # search = driver.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
+    # search.clear()
+    # search.send_keys("天府穴" + "再探針灸大成")
+    # search.send_keys(Keys.RETURN)
     time.sleep(2)
     
-    for i in range(0,1):
-        ch = driver.find_elements_by_class_name("LC20lb")[i]
-        ch.click()
-        time.sleep(2)
+    # for i in range(0,1):
+    #     ch = driver.find_elements_by_class_name("LC20lb")[i]
+    #     ch.click()
+    #     time.sleep(2)
         
-        driver.back()
-        time.sleep(2)
+    #     driver.back()
+    #     time.sleep(2)
+        
+    f.close()
         
 Task()
-
 
 driver.quit()
 
