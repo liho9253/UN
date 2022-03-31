0f5e714150f730540137d8987ce6fbb5da220e28
liho9253 <timmy89566@gmail.com>
2022-03-31 14:44:31 +0800

    20220331

--- test/compare.py
+++ test/compare.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 
-df1 = pd.read_csv("text.csv", encoding='utf-8')
+df1 = pd.read_csv("two.txt", encoding='utf-8').rename(columns={'course_title':'name'})
 
-print(df1)
+
+df2 = pd.read_csv("two.txt", encoding='utf-8')
+
+result = df1.merge(df2, how='inner')
+print(df2)
 
 
 
