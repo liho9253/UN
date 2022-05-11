a238dc6080fe506b1bc8b0838872082574a270ab
liho9253 <timmy89566@gmail.com>
2022-05-11 21:21:50 +0800

    20220511

--- test/z2.py
+++ test/z2.py
@@ -1,7 +1,8 @@
 from FET_user import User
 from FET_user import db
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_paginate import Pagination, get_page_args
+from sqlalchemy import or_
 import pandas
 
 app = Flask(__name__)
@@ -16,43 +17,63 @@ df = pandas.read_csv('test16.csv')
 df.replace("\r\n",'<br>', inplace=True,regex = True)
 data = df.to_dict(orient = 'records')
 
-def get_page(offset=0, per_page=10):
-    return data[offset: offset + per_page]
+for row in data:
+    if (row['SR_ID'] != ""):
+        continue
+            
+    inf = User(row['SR_ID'],
+               row['PLAN_START_DATE_TEXT'],
+               row['PLAN_END_DATE_TEXT'],
+               row['SUBJECT'],
+               row['SPECIALIST_NAME'],
+               row['CLOSE_DATE_TEXT'],
+               row['CREATOR_WORKGROUP_CODE'],
+               row['SR_SUB_CATEGORY'],
+               row['COX_TEXT'],
+               row['DESCRIPTION'],
+               row['CREATOR_NAME'])
+    db.session.add(inf)
+    db.session.commit()
 
+qu = User.query.all()
 
+def get_page(offset=0, per_page=10, qu=qu):
+    return qu[offset: offset + per_page]
 @app.route('/')
-def index():
-    for row in data:
-        if (row['SR_ID'] != ""):
-            continue
-                
-        inf = User(row['SR_ID'],
-                   row['PLAN_START_DATE_TEXT'],
-                   row['PLAN_END_DATE_TEXT'],
-                   row['SUBJECT'],
-                   row['SPECIALIST_NAME'],
-                   row['CLOSE_DATE_TEXT'],
-                   row['CREATOR_WORKGROUP_CODE'],
-                   row['SR_SUB_CATEGORY'],
-                   row['COX_TEXT'],
-                   row['DESCRIPTION'],
-                   row['CREATOR_NAME'])
-        db.session.add(inf)
-        db.session.commit()
-        
-    query = User.query.all()
-    
-    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-    total = len(data)
 
+def index():
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
+    pagination_users = get_page(offset=offset, per_page=per_page)
+    
+    # 頁數
+    total = len(qu)
     pagination = Pagination(page=page, 
                             per_page=per_page, 
                             total=total,
                             css_framework='bootstrap4')
+    
     return render_template('FET_main.html',
-                            query=query,
+                            qu=pagination_users,
                             pagination=pagination)
 
+
+@app.route('/search',methods=['GET','POST'])
+def search():
+    se = request.form['username'].strip()
+    qu = User.query.filter_by(ID=se).all()
+    
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
+    pagination_users = get_page(offset=offset, per_page=per_page, qu=qu)
+    
+    total = len(qu)
+    pagination = Pagination(page=page, 
+                            per_page=per_page, 
+                            total=total,
+                            css_framework='bootstrap4')
+    
+    return render_template('FET_main.html',
+                            qu=pagination_users,
+                            pagination=pagination)
 
 if __name__ == "__main__":
     
