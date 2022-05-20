212d05def3120ba37334355c5517993403edb95c
liho9253 <timmy89566@gmail.com>
2022-05-20 15:57:49 +0800

    20220520

--- test/mail.py
+++ test/mail.py
@@ -1,40 +1,35 @@
 from flask import Flask
-from apscheduler.schedulers.background import BackgroundScheduler
-from flask_apscheduler import APScheduler
+from flask_mail import Mail, Message
 
+app = Flask(__name__)
 
+app.config.update(    
+    MAIL_SERVER='timmy89566@gmail.com',
+    MAIL_PROT=587,
+    MAIL_USE_TLS=True,
+    MAIL_USERNAME='曾偉國',
+    MAIL_PASSWORD='as785241963')
 
-class Config(object):
-    JOBS = [
-        {
-            'id': 'job1',
-            'func': 'mail:add',
-            'args': (1, 2),
-            'trigger': 'interval',
-            'seconds': 3
-        }
-    ]
-    SCHEDULER_TIMEZONE = 'Asia/Shanghai'
-    SCHEDULER_API_ENABLED = True
+mail = Mail(app)
 
-scheduler = APScheduler(BackgroundScheduler(timezone="Asia/Shanghai"))
-
-def add(a, b):
-    print(str(a)+' '+str(b))
-
-# @scheduler.task('interval', id='job_2', seconds=30, misfire_grace_time=900)
-# def job2():
-#     print('Job 2 executed')
-
-
-if __name__ == '__main__':
-    app = Flask(__name__)
-    app.config.from_object(Config())
+@app.route("/message")
+def index():
+    msg_title = "Test"
+    msg_sender = "曾偉國 timmy89566@gmail.com"
+    msg_recipients = ['timmy89566@gmail.com']
+    msg_body = "測試"
     
-    scheduler.init_app(app)
-    scheduler.start()
+    msg = Message(msg_title,
+                  sender=msg_sender,
+                  recipients=msg_recipients)
+    msg.body = msg_body
+
+    mail.send(msg)
+    return 'You Send Mail by Flask-Mail Success!!'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
     
-    app.run()
-
-
-
+    
+    
+    
