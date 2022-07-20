616e8101f5c5a86853165df62464c8104e755b92
liho9253 <timmy89566@gmail.com>
2022-07-20 20:47:00 +0800

    20220720

--- test/mail.py
+++ test/mail.py
@@ -1,54 +1,28 @@
-import smtplib
+# -- coding: utf-8 --
+from apscheduler.schedulers.blocking import BlockingScheduler
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.header import Header
-import os
+import smtplib
+from string import Template
+from path import Path 
 
-# os.environ['http_proxy'] = "http://127.0.0.1"
-# os.environ['https_proxy'] = "https://127.0.0.1"
-sender = 'from@gmail.com'
-receivers = ['timmy89566@gmail.com']
-message = MIMEText('Python 測試...', 'plain', 'utf-8')
-message['From'] = Header("123", 'utf-8')   
-message['To'] =  Header("測試", 'utf-8')     
- 
-subject = 'Python SMTP 測試'
-message['Subject'] = Header(subject, 'utf-8')
+content = MIMEMultipart()  #建立MIMEMultipart物件
+content["subject"] = "test"  #郵件標題
+content["from"] = "108111123@mail.aeust.edu.tw"  #寄件者
+content["to"] = "108111123@mail.aeust.edu.tw" #收件者
+template = Template(Path("mail_template.html").read_text())
+body = template.substitute({ "user": "Mike" })
+content.attach(MIMEText(body, "html"))
 
+sched = BlockingScheduler() 
 
-smtpObj = smtplib.SMTP('localhost', 25)
-smtpObj.sendmail(sender, receivers, message.as_string())
-try:
-    print("成功")
-except smtplib.SMTPException:
-    print("Error")
-
-# import smtplib
-# from email.mime.text import MIMEText 
-# smtpHost = 'smtp.exmail.qq.com' 
-# sender = '<a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="dfadb0bdb0ab9fb2b6b2a9aff1bcb0b2">[email protected]</a>' 
-# password = "mimvp-password" 
-# receiver = '<a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="7f061e1118181e11183f121612090f511c1012">[email protected]</a>'
-# content = 'hello mimvp.com' 
-# msg = MIMEText(content) 
-# msg['Subject'] = 'email-subject' 
-# msg['From'] = sender 
-# msg['To'] = receiver 
-   
-
-## smtp port 25
-smtpServer = smtplib.SMTP(smtpHost, 25)         # SMTP
-smtpServer.login(sender, password) 
-smtpServer.sendmail(sender, receiver, msg.as_string()) 
-smtpServer.quit() 
-print('send success by port 25' )
- 
-## smtp ssl port 465
-smtpServer = smtplib.SMTP_SSL(smtpHost, 465)    # SMTP_SSL
-smtpServer.login(sender, password) 
-smtpServer.sendmail(sender, receiver, msg.as_string()) 
-smtpServer.quit() 
-print('send success by port 465' )
-
-
-
-
+def mail():
+    with smtplib.SMTP(host="smtp.office365.com", port="587") as smtp:  # 設定SMTP伺服器
+        try:
+            smtp.ehlo()  # 驗證SMTP伺服器
+            smtp.starttls()  # 建立加密傳輸
+            smtp.login("帳號", "密碼")  # 
+            smtp.send_message(content)  # 寄送郵件
+            print("Complete!")
+        except Exception as e:
+            print("Error message: ", e)
--- test/try.py
+++ test/try.py
@@ -1,7 +1,25 @@
+import configparser
+import pandas
+def pands_csv():
+    df = pandas.read_csv("test16.csv")
+    df.replace("\r\n",'<br>', inplace=True,regex = True)
+    df.replace("1(High)",'Critical', inplace=True)
+    df.replace("2(Medium)",'Major', inplace=True)
+    df.replace("3(Low)",'Minor', inplace=True)
+    data = df.to_dict(orient = 'records')
+    return data
+def pands_excel():
+    df = pandas.read_excel("SRTT.xls")
+    df.replace("",'', inplace=True,regex = True)
+    data_excel = df.to_dict(orient = 'records')
+    return data_excel
+
+data=pands_csv()
+data1=pands_excel()
 
 
-a = list("大小,小鐘")
-b = list("小鐘,小鐘")
+def get_page(offset=0, per_page=10):
+    return data[offset: offset + per_page]
 
-print(a.sort())
-print(b.sort())
+def get_page1(offset=0, per_page=10):
+    return data1[offset: offset + per_page]
