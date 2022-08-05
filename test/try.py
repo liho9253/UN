11434bb930fcddfeb6e7d6285072944ff847c2ca
liho9253 <timmy89566@gmail.com>
2022-08-05 22:19:10 +0800

    20220805

--- test/try.py
+++ test/try.py
@@ -1,28 +1,10 @@
-import os,time
-def close():
-  print("程式重啟！！！！")
-  print(time).strftime('%Y.%m.%d-%H.%M.%S')
-  time.sleep(2) #3秒
-  p = os.popen('11111111.bat')
-  while True:
-    line = p.readline();
-    if '' == line:
-      break
-    print(line)
-if __name__ == '__main__':
-  close()
+import os
+import sys
 
+restart = input("\nDo you want to restart the program? [y/n] > ")
 
-
-
-# import time
-# import sys
-# import os
-# def restart_program():
-#     python = sys.executable
-#     os.execl(python, python, * sys.argv)
-# if __name__ == "__main__":
-#     print('start...')
-#     print(u"3秒後,程式將結束...").encode("gbk")
-#     time.sleep(3)
-#     restart_program()
+if restart == "y":
+    os.execl(sys.executable, os.path.abspath(__file__), *sys.argv) 
+else:
+    print("\nThe program will be closed...")
+    sys.exit(0)
