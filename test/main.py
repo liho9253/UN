83f880406da56b3a9a76afe8ad4ed7c3434e3d6c
liho9253 <timmy89566@gmail.com>
2022-08-15 00:55:17 +0800

    20220815

--- test/main.py
+++ test/main.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, request, session
 from flask_paginate import Pagination, get_page_args
 from sqlalchemy import or_
 import pandas as pd
-import pandas, os, sys
+import pandas, os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
@@ -203,8 +203,119 @@ def search():
 
 @app.route('/update',methods=['GET','POST'])
 def update():
-    python = sys.executable
-    os.execl(python, python, * sys.argv)
+    
+    if(path_csv):
+        df = pd.read_csv('order/SR-Sample.csv', encoding='big5')
+        df.replace("\r\n",'<br>', inplace=True,regex = True)
+        df.replace("\n",'<br>', inplace=True,regex = True)
+        df.replace("1(High)",'Critical', inplace=True)
+        df.replace("2(Medium)",'Major', inplace=True)
+        df.replace("3(Low)",'Minor', inplace=True)
+        df = df.dropna()
+        df = df.drop_duplicates(subset=["SR_ID"], keep="last")
+        data_csv = df.to_dict(orient = 'records')
+        for row in data_csv:
+            row['Major'] = 0
+            row['State'] = "In Coming"
+            inf = User( row['SR_ID'],
+                        row['PLAN_START_DATE_TEXT'],
+                        row['PLAN_END_DATE_TEXT'],
+                        row['SUBJECT'],
+                        row['SPECIALIST_NAME'],
+                        row['CREATOR_WORKGROUP_CODE'],
+                        row['SR_SUB_CATEGORY'],
+                        row['COX_TEXT'],
+                        row['CREATOR_NAME'],
+                        row['Major'],
+                        row['State'])
+            if (User.query.filter_by(ID=str(row['SR_ID'])).all() != None):
+                inf_db = (User.query.filter_by(ID=str(row['SR_ID'])).all())
+                try:
+                    if inf_db[0].ID != row['SR_ID']:
+                        inf_db[0].ID = row['SR_ID']
+                    if inf_db[0].StartDate != row['PLAN_START_DATE_TEXT']:
+                        inf_db[0].StartDate = row['PLAN_START_DATE_TEXT']
+                    if inf_db[0].EndDate != row['PLAN_END_DATE_TEXT']:
+                        inf_db[0].EndDate = row['PLAN_END_DATE_TEXT']
+                    if inf_db[0].Sub != row['SUBJECT']:
+                        inf_db[0].Sub = row['SUBJECT']
+                    if inf_db[0].SpN != row['SPECIALIST_NAME']:
+                        inf_db[0].SpN = row['SPECIALIST_NAME']
+                    if inf_db[0].CreWGro != row['CREATOR_WORKGROUP_CODE']:
+                        inf_db[0].CreWGro = row['CREATOR_WORKGROUP_CODE']
+                    if inf_db[0].SR != row['SR_SUB_CATEGORY']:
+                        inf_db[0].SR = row['SR_SUB_CATEGORY']
+                    if inf_db[0].CoxT != row['COX_TEXT']:
+                        inf_db[0].CoxT = row['COX_TEXT']
+                    if inf_db[0].CreN != row['CREATOR_NAME']:
+                        inf_db[0].CreN = row['CREATOR_NAME']
+                    if inf_db[0].Major != 0:
+                        inf_db[0].Major = inf_db[0].Major
+                    if inf_db[0].State != "In Coming":
+                        inf_db[0].State = inf_db[0].State
+                    db.session.commit()
+                except IndexError:
+                    db.session.add(inf)
+                    db.session.commit()
+            else:
+                db.session.add(inf)
+                db.session.commit()
+                
+    if(path_excel):
+        df = pandas.read_excel("order/SRTT.xls")
+        df.replace("",'', inplace=True,regex = True)
+        data_excel = df.to_dict(orient = 'records')
+        for row in data_excel:
+            row['Major'] = 0
+            row['State'] = "In Coming"
+            inf = User( row['SR編號'],
+                        row['開始日期'],
+                        row['結束日期'],
+                        row['測試專案名稱'],
+                        row['收單者'],
+                        row['申請部門'],
+                        row['需求分類'],
+                        row['優先順序'],
+                        row['SR開單者'],
+                        row['Major'],
+                        row['State'])
+            if (User.query.filter_by(ID=str(row['SR編號'])).all() != None):
+                inf_db = (User.query.filter_by(ID=str(row['SR編號'])).all())
+                try:
+                    if inf_db[0].ID != row['SR編號']:
+                        inf_db[0].ID = row['SR編號']
+                    if inf_db[0].StartDate != row['開始日期']:
+                        inf_db[0].StartDate = row['開始日期']
+                    if inf_db[0].EndDate != row['結束日期']:
+                        inf_db[0].EndDate = row['結束日期']
+                    if inf_db[0].Sub != row['測試專案名稱']:
+                        inf_db[0].Sub = row['測試專案名稱']
+                    if inf_db[0].SpN != row['收單者']:
+                        inf_db[0].SpN = row['收單者']
+                    if inf_db[0].CreWGro != row['申請部門']:
+                        inf_db[0].CreWGro = row['申請部門']
+                    if inf_db[0].SR != row['需求分類']:
+                        inf_db[0].SR = row['需求分類']
+                    if inf_db[0].CoxT != row['優先順序']:
+                        inf_db[0].CoxT = row['優先順序']
+                    if inf_db[0].CreN != row['SR開單者']:
+                        inf_db[0].CreN = row['SR開單者']
+                    if inf_db[0].Major != 0:
+                        inf_db[0].Major = inf_db[0].Major
+                    if inf_db[0].State != "In Coming":
+                        inf_db[0].State = inf_db[0].State
+                    db.session.commit()
+                except IndexError:
+                    db.session.add(inf)
+                    db.session.commit()
+            else:
+                db.session.add(inf)
+                db.session.commit()
+            
+    qu = User.query.order_by("ID")
+    
+    def get_page(offset=0, per_page=10, qu=qu):
+        return qu[offset: offset + per_page]
 
     qu = User.query.all()
     total = len(qu)
