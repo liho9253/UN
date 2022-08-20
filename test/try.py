0f050dd27d09183dc0ebfeebf1e1c9f7631e5879
liho9253 <timmy89566@gmail.com>
2022-08-20 16:35:10 +0800

    20220820

--- test/try.py
+++ test/try.py
@@ -10,7 +10,28 @@ config['http']['port'] = str(port)
 
 
 
-with open('config.ini', 'r') as f:
-    print(f.readlines()[2])
-    print(f.readlines()[0])
+with open('config.ini', 'w') as f:
+    config.write(f)
     
+c
+
+print(config['http']['host'])
+
+print(config.sections())
+
+
+
+
+# with open('config.ini', 'r') as f:
+#     htT1 = f.readlines()[1]
+    
+# with open('config.ini', 'r') as f:
+#     htT2 = f.readlines()[2]
+
+
+# print(htT1)
+# print(htT2)
+
+# with open('config.ini', 'w') as f:
+#     config['http']['host'] = htT1
+#     config.write(f)
