dc54f3dca2af4ce1d87fd1bd43f81d989983a8dc
liho9253 <timmy89566@gmail.com>
2022-10-18 16:01:22 +0800

    20221018

--- acupuncture/hand1.py
+++ acupuncture/hand1.py
@@ -22,28 +22,28 @@ while True:
             for handLms in result.multi_hand_landmarks:
                 Draw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
                 for i, lm in enumerate(handLms.landmark):
-                    # xPos = int(lm.x * imgWidth)
-                    # yPos = int(lm.y * imgHeight)
+                    xPos = int(lm.x * imgWidth)
+                    yPos = int(lm.y * imgHeight)
                     
-                    # cv2.putText(img, str(i), (xPos-25, yPos+5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 2)
-                    # print(i, xPos, yPos)
+                    cv2.putText(img, str(i), (xPos-25, yPos+5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 2)
+                    print(i, xPos, yPos)
                     
-                    if ( i == 6 ) | ( i == 7 ):
-                        xPos = int(lm.x * imgWidth)
-                        yPos = int(lm.y * imgHeight)
-                        if i == 6:
-                            xp6 =  int(lm.x * imgWidth)
-                            yp6 =  int(lm.y * imgHeight)
-                            zp6 =  int(lm.z)
-                        if i == 7:
-                            xp7 =  int(lm.x * imgWidth)
-                            yp7 =  int(lm.y * imgHeight)
-                            zp7 =  int(lm.z)
-                        cv2.putText(img, str(i), (xPos-25, yPos+5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 2)
+                    # if ( i == 6 ) | ( i == 7 ):
+                    #     xPos = int(lm.x * imgWidth)
+                    #     yPos = int(lm.y * imgHeight)
+                    #     if i == 6:
+                    #         xp6 =  int(lm.x * imgWidth)
+                    #         yp6 =  int(lm.y * imgHeight)
+                    #         zp6 =  int(lm.z)
+                    #     if i == 7:
+                    #         xp7 =  int(lm.x * imgWidth)
+                    #         yp7 =  int(lm.y * imgHeight)
+                    #         zp7 =  int(lm.z)
+                    #     cv2.putText(img, str(i), (xPos-25, yPos+5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 2)
                         
                 # len67 = ((xp6-xp7)**2+(yp6-yp7)**2)**0.5
-                len67 = ((xp6-xp7)**2+(yp6-yp7)**2+(zp6-zp7)**2)**0.5
-                print(int(len67))
+                # len67 = ((xp6-xp7)**2+(yp6-yp7)**2+(zp6-zp7)**2)**0.5
+                # print(int(len67))
                 
         cv2.imshow("img", img)
     
