6e5a1522215ca17a738f3cdba920e46800f223cd
liho9253 <timmy89566@gmail.com>
2022-10-24 16:13:48 +0800

    20221024

--- acupuncture/coin_hand.py
+++ acupuncture/coin_hand.py
@@ -4,7 +4,7 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-IMAGE_FILES = [".\img\h29.jpg"]
+IMAGE_FILES = [".\img\h20.jpg"]
 with mp_hands.Hands(
     static_image_mode=True,
     max_num_hands=2,
@@ -47,8 +47,13 @@ with mp_hands.Hands(
         # coinlen = w / 96 * 25.4
         cv2.imshow('imgContour', rec)
         print("硬幣: " + str(round((w/2.6) / (w/2.6) *25, 1)))
-        prop = (w/2.6) / (w/2.6) *25
-                
+        prop = (w/2.6)
+        
+        # (w/2.6) : len67/(w/2.6) = 25 : X
+        # X = len67/(w/2.6) *25 / (w/2.6)
+        
+        
+        
         imgHeight = image.shape[0]
         imgWidth = image.shape[1]
         results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
@@ -66,13 +71,13 @@ with mp_hands.Hands(
                       xp6 =  int(lm.x * imgWidth)
                       yp6 =  int(lm.y * imgHeight)
                       zp6 =  int(lm.z)
-                  if i == 11:
+                  elif i == 11:
                       xp7 =  int(lm.x * imgWidth)
                       yp7 =  int(lm.y * imgHeight)
                       zp7 =  int(lm.z)
                       
           len67 = ((xp6-xp7)**2+(yp6-yp7)**2+(zp6-zp7)**2)**0.5
-          inc = str(round(len67/prop, 2))
+          inc = str(round(len67/prop/ (w/2.6) * 25, 2))
           print("1吋 = " + inc)
           mp_drawing.draw_landmarks(
               annotated_image,
